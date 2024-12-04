d636548a12c9a16ac68d003c78ad919ab9c8d504
Meet Thakkar <meetthakkar05@gmail.com>
2024-12-03 19:24:53 -0800

    DB Dialect Changes for LAmbda Deployment

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,12 +4,12 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     logging: false,
-    // dialectOptions: {
-    //     ssl: {
-    //         require: true, // This will help you. But you will see nwe error
-    //         rejectUnauthorized: false // This line will fix new error
-    //     }
-    // },
+    dialectOptions: {
+        ssl: {
+            require: true, // This will help you. But you will see nwe error
+            rejectUnauthorized: false // This line will fix new error
+        }
+    }
 });
 
 const connectDB = async () => {
